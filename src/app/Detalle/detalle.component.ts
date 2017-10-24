d06e1380b8c367fdb7a2d78113d13ec147119e1a
mac160382 <mac160382@gmail.com>
2017-10-23 19:27:29 -0500

    Creando una vistas de detalle para el proyectog

--- src/app/Detalle/detalle.component.ts
+++ src/app/Detalle/detalle.component.ts
@@ -6,10 +6,27 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './detalle.component.html'  
 })
 export class DetalleComponent {
+  id: number;
+  lugar:any = {};
+  lugares:any = [
+    {id:1, plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre:'Donas la padadita', descripcion: 'Descripcion de este negocio. Más adelante tendremos informacion' },
+    {id:2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre:'Veterinaria Huelliltas Felices', descripcion: 'Descripcion de este negocio. Más adelante tendremos informacion'},
+    {id:3, plan: 'gratuito', cercania: 2, distancia: 5, active: true, nombre:'Floreria la Gardenia',  descripcion: 'Descripcion de este negocio. Más adelante tendremos informacion'},
+  ];
+
   constructor(private router: ActivatedRoute){
     console.log(this.router.snapshot.params['id']);
     console.log(this.router.snapshot);
     console.log(this.router.snapshot.queryParams['action2']);
     console.log(this.router.snapshot.queryParams['referer']);
+
+    this.id = this.router.snapshot.params['id'];
+    this.lugar = this.getSelecterItem();
+    console.log(this.lugar);    
   }  
+
+  getSelecterItem()
+  {
+    return this.lugares.filter((lugar) => {return lugar.id == this.id })[0] || null;
+  }
 }
